83a7d8bb31024ecb5155f6236af6a5d17edeb97f
unknown <raziman.terra@gmail.com>
2019-06-10 23:05:57 +0800

    info team6

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let xp = require('./xp.json');
 bot.on('ready', () =>{
     console.log('This bot is online!');
     bot.user.setActivity('Habbo');
+    
 })
 
 bot.on('guildMemberAdd', member =>{
@@ -50,58 +51,60 @@ bot.on('guildMemberUpdate',(oldMember, newMember) =>{
 });
 
 
-bot.on('message', msg, member=>{
+bot.on('message', msg =>{
 
-    let myRole = member.guild.roles.find(role => role.name === "Malaysia");
-    let sgRole = member.guild.roles.find(role => role.name === "Singapore");
+    
 
     let args = msg.content.substring(prefix.length).split(" ");
+    
 
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
     switch(args[0]){
         case 'ping':
 
-            msg.channel.sendMessage('pong!')
+            msg.channel.send('pong!')
         break;
         case 'website':
-            msg.channel.sendMessage('youtube.com')
+            msg.channel.send('youtube.com')
         break;
         case 'info':
+            let myRole = msg.guild.roles.find(role => role.name === "Malaysia");
+            let sgRole = msg.guild.roles.find(role => role.name === "Singapore");
+
+            let roleInfo = msg.mentions.roles.first();
             
-            args[1] = msg.content.role
-            if (args[1] == 0){
+            if (!args[1]){
                 return msg.reply('You need to specify which team')
             }
 
-            else if (args[1] !== myRole || sgRole){
-                return msg.reply("That's an invalid role")
-            }
+        
 
-            else if (args[1] === myRole || sgRole){
             
+            if (roleInfo.id == myRole.id){
             const embed = new Discord.RichEmbed()
             .setTitle('Team Information')
-            .addField('Team', msg.guild.role.name)
+            .addField('Team', roleInfo)
             .setTimestamp()
-            
-            if(args[1] === myRole){
-                const embed = new Discord.RichEmbed()
-                .setThumbnail({files: ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.affsuzukicup.com%2F2018%2Fimages%2Fflags%2Fmalaysia.png&imgrefurl=https%3A%2F%2Fwww.affsuzukicup.com%2F&docid=6IeO630wmweBWM&tbnid=uJB99oPzMZ12CM%3A&vet=10ahUKEwjbsv_OxN7iAhXMfSsKHadbDs8QMwhHKAAwAA..i&w=239&h=319&bih=959&biw=851&q=malaysia%20aff%20logo&ved=0ahUKEwjbsv_OxN7iAhXMfSsKHadbDs8QMwhHKAAwAA&iact=mrc&uact=8"]})
-                .setColor('4286f4')
-                .addField('Team member', myRole.members.size)
-            }
-            else if(args[1] === sgRole){
-                const embed = new Discord.RichEmbed()
-                .setThumbnail({files: ["https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.affsuzukicup.com%2F2018%2Fimages%2Fflags%2Fsingapore.png&imgrefurl=https%3A%2F%2Fwww.affsuzukicup.com%2F2018%2Fteams%2Fsingapore&docid=siyTMers73MlgM&tbnid=G14U1ObFqNFAnM%3A&vet=10ahUKEwiCs-XBxt7iAhVFP48KHfRyCDQQMwhYKBEwEQ..i&w=239&h=319&bih=959&biw=851&q=singapore%20aff%20logo&ved=0ahUKEwiCs-XBxt7iAhVFP48KHfRyCDQQMwhYKBEwEQ&iact=mrc&uact=8"]})
-                .setColor('ff2121')
-                .addField('Team member', sgRole.members.size)
-            };
-            
-            
+            .setThumbnail('https://i.imgur.com/0ZK52WE.png')
+            .setColor('4286f4')
+            .addField('Team member', myRole.members.size)
+            msg.channel.send(embed);
+             }
 
-            msg.channel.sendEmbed(embed);
+            if (roleInfo.id == sgRole.id){
+            const embed = new Discord.RichEmbed()
+            .setTitle('Team Information')
+            .addField('Team', roleInfo)
+            .setTimestamp()
+            .setThumbnail('https://i.imgur.com/x3KTH7M.png')
+            .setColor('ff2121')
+            .addField('Team member', sgRole.members.size)
+            msg.channel.send(embed);
             };
+        
+            
+            
             
         break;
         case 'clear':
