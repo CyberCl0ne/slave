4a3494a48c522459d505ad0f4aaf06c6e9985edd
unknown <raziman.terra@gmail.com>
2019-06-22 00:06:15 +0800

    emotes

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ bot.on('message', msg =>{
         let detail = 'Blood, blood everywhere'
         let color = 'bb0a1e'
         let thumbnail = 'https://i.pinimg.com/originals/5e/59/f6/5e59f6475951584c42ff751e8d748f66.gif'
-        let footer = `Fired by ${author}`
+        let footer = `Shot by ${author}`
         return emotes(title, field, detail, color, thumbnail, footer);
     }
        
@@ -250,9 +250,50 @@ bot.on('message', msg =>{
         return emotes(title, field, detail, color, thumbnail, footer);
     }
 
+    if(args[0] == 'slap'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to **slap**!');
+        }
+        
+        let title = 'My hand is coming!'
+        let field = ` ${memberInfo.displayName}, you were just slapped by ${author} `
+        let detail = 'auchh'
+        let color = 'ff69b4'
+        let thumbnail = 'https://media3.giphy.com/media/xT9IgzTnZHL9zp6IPS/source.gif'
+        let footer = `Slapped by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
 
+    if(args[0] == 'fart'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to fart 💨');
+        }
+        
+        let title = 'Smell this!'
+        let field = ` ${memberInfo.displayName}, you smelled ${author}'s fart `
+        let detail = 'serves you well 💨'
+        let color = 'ff69b4'
+        let thumbnail = 'https://media.tenor.com/images/074a0cd24138a6260391f90efabdab5c/tenor.gif'
+        let footer = `Farted by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
 
-
+    if(args[0] == 'kick'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to **kick**!');
+        }
+        
+        let title = 'Parry this!'
+        let field = ` ${memberInfo.displayName}, you were kicked at the butt by ${author}`
+        let detail = 'Right to the butt'
+        let color = 'ff69b4'
+        let thumbnail = 'https://media.tenor.com/images/27f16871c55a3376fa4bfdd76ac2ab5c/tenor.gif'
+        let footer = `Kicked by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
     
 
 
