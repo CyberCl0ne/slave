75d7bd92ff526a76ed4d74b9449836311742f6ef
unknown <raziman.terra@gmail.com>
2019-06-22 19:56:41 +0800

    lmao

---
+++ emotes.js
@@ -0,0 +1,92 @@
+module.exports = (msg, args, memberInfo, Discord) => {
+
+   
+
+    function emotes(title, field, detail, color, thumbnail, footer){
+        const embed = new Discord.RichEmbed()
+        .setTitle(title)
+        .addField(field, detail)
+        .setColor(color)
+        .setThumbnail(thumbnail)
+        .setTimestamp()
+        .setFooter(footer)
+        return msg.channel.send(embed);
+    }
+    
+    if(args[0] == 'shoot'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to kill 😈');
+        }
+        
+        let title = 'Shots fired!'
+        let field = ` ${memberInfo.displayName}, you were just shot by ${author} 🔫`
+        let detail = 'Blood, blood everywhere'
+        let color = 'bb0a1e'
+        let thumbnail = 'https://i.pinimg.com/originals/5e/59/f6/5e59f6475951584c42ff751e8d748f66.gif'
+        let footer = `Shot by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
+       
+    if(args[0] == 'hug'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to hug️ ❤️');
+        }
+        
+        let title = 'Giving you a big hug!'
+        let field = ` ${memberInfo.displayName}, you were just hugged by ${author} ❤️`
+        let detail = '😍'
+        let color = 'ff69b4'
+        let thumbnail = 'https://i.pinimg.com/originals/42/94/de/4294deb5ec97086243174b085d609695.gif'
+        let footer = `Hugged by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
+
+    if(args[0] == 'slap'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to **slap**!');
+        }
+        
+        let title = 'My hand is coming!'
+        let field = ` ${memberInfo.displayName}, you were just slapped by ${author} `
+        let detail = 'auchh'
+        let color = 'f4be41'
+        let thumbnail = 'https://media3.giphy.com/media/xT9IgzTnZHL9zp6IPS/source.gif'
+        let footer = `Slapped by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
+
+    if(args[0] == 'fart'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to fart 💨');
+        }
+        
+        let title = 'Smell this!'
+        let field = ` ${memberInfo.displayName}, you smelled ${author}'s fart `
+        let detail = 'Serves you well 💨'
+        let color = '26ba21'
+        let thumbnail = 'https://media.tenor.com/images/074a0cd24138a6260391f90efabdab5c/tenor.gif'
+        let footer = `Farted by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
+
+    if(args[0] == 'kick'){
+        let author = msg.guild.members.get(msg.author.id).displayName;
+        if(!memberInfo){
+            return msg.channel.send('You need to mention which user you want to **kick**!');
+        }
+        
+        let title = 'Parry this!'
+        let field = ` ${memberInfo.displayName}, you were kicked at the butt by ${author}`
+        let detail = 'Right to the butt'
+        let color = 'f47a42'
+        let thumbnail = 'https://media.tenor.com/images/27f16871c55a3376fa4bfdd76ac2ab5c/tenor.gif'
+        let footer = `Kicked by ${author}`
+        return emotes(title, field, detail, color, thumbnail, footer);
+    }
+    
+
+};
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var data = require('./data.json');
 const fs = require('fs');
 const reactions = require('./reactions.js');
 const talkedRecently = new Set();
+const emotes = require('./emotes.js');
 
 
                
@@ -74,12 +75,14 @@ bot.on('message', msg =>{
 
     const thisChannel = msg.guild.channels.find(channel => channel.name === "🤖bot-commands");
     let args = msg.content.substring(prefix.length).split(" ");
-   
     
+   
+  
     reactions(msg);
 
     if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 
+    emotes(msg, args, memberInfo, Discord);
   
     if(!args[0]) return;
 
@@ -96,13 +99,13 @@ bot.on('message', msg =>{
         .addField('?info @user', 'Displays info about mentioned user', true)      
         .addField('?mood', 'Sets your current mood', true)
         .addField('?ping', 'Gives you ping result for the bot', true)
-        .addField('?respect @user ✔️', 'Show your gratitude by giving them respect')
-        .addField('?shoot @user ✔️', 'If you can only kill them irl', true)
-        .addField('?hug @user ✔️', 'Hug a person', true)
-        .addField('?kick @user ✔️', 'Kick those butts', true)
-        .addField('?slap @user ✔️', 'It feels good tho', true)
-        .addField('?fart @user ✔️', 'Let them smell', true)
-        .addField('Addition', '"✔️" shows the commands that can be used in any channel')
+        .addField('?respect @user ✔', 'Show your gratitude by giving them respect')
+        .addField('?shoot @user ✔', 'If you can only kill them irl', true)
+        .addField('?hug @user ✔', 'Hug a person', true)
+        .addField('?kick @user ✔', 'Kick those butts', true)
+        .addField('?slap @user ✔', 'It feels good tho', true)
+        .addField('?fart @user ✔', 'Let them smell', true)
+        .addField('Addition', '"✔" shows the commands that can be used in any channel')
         .setThumbnail('https://i.imgur.com/iwewYsx.png')
         .setTimestamp()
         .setColor('24E2E7')
@@ -160,93 +163,7 @@ bot.on('message', msg =>{
         return
     }
 
-    function emotes(title, field, detail, color, thumbnail, footer){
-        const embed = new Discord.RichEmbed()
-        .setTitle(title)
-        .addField(field, detail)
-        .setColor(color)
-        .setThumbnail(thumbnail)
-        .setTimestamp()
-        .setFooter(footer)
-        return msg.channel.send(embed);
-    }
-    
-    if(args[0] == 'shoot'){
-        let author = msg.guild.members.get(msg.author.id).displayName;
-        if(!memberInfo){
-            return msg.channel.send('You need to mention which user you want to kill 😈');
-        }
-        
-        let title = 'Shots fired!'
-        let field = ` ${memberInfo.displayName}, you were just shot by ${author} 🔫`
-        let detail = 'Blood, blood everywhere'
-        let color = 'bb0a1e'
-        let thumbnail = 'https://i.pinimg.com/originals/5e/59/f6/5e59f6475951584c42ff751e8d748f66.gif'
-        let footer = `Shot by ${author}`
-        return emotes(title, field, detail, color, thumbnail, footer);
-    }
-       
-    if(args[0] == 'hug'){
-        let author = msg.guild.members.get(msg.author.id).displayName;
-        if(!memberInfo){
-            return msg.channel.send('You need to mention which user you want to hug️ ❤️');
-        }
-        
-        let title = 'Giving you a big hug!'
-        let field = ` ${memberInfo.displayName}, you were just hugged by ${author} ❤️`
-        let detail = '😍'
-        let color = 'ff69b4'
-        let thumbnail = 'https://i.pinimg.com/originals/42/94/de/4294deb5ec97086243174b085d609695.gif'
-        let footer = `Hugged by ${author}`
-        return emotes(title, field, detail, color, thumbnail, footer);
-    }
-
-    if(args[0] == 'slap'){
-        let author = msg.guild.members.get(msg.author.id).displayName;
-        if(!memberInfo){
-            return msg.channel.send('You need to mention which user you want to **slap**!');
-        }
-        
-        let title = 'My hand is coming!'
-        let field = ` ${memberInfo.displayName}, you were just slapped by ${author} `
-        let detail = 'auchh'
-        let color = 'f4be41'
-        let thumbnail = 'https://media3.giphy.com/media/xT9IgzTnZHL9zp6IPS/source.gif'
-        let footer = `Slapped by ${author}`
-        return emotes(title, field, detail, color, thumbnail, footer);
-    }
-
-    if(args[0] == 'fart'){
-        let author = msg.guild.members.get(msg.author.id).displayName;
-        if(!memberInfo){
-            return msg.channel.send('You need to mention which user you want to fart 💨');
-        }
-        
-        let title = 'Smell this!'
-        let field = ` ${memberInfo.displayName}, you smelled ${author}'s fart `
-        let detail = 'Serves you well 💨'
-        let color = '26ba21'
-        let thumbnail = 'https://media.tenor.com/images/074a0cd24138a6260391f90efabdab5c/tenor.gif'
-        let footer = `Farted by ${author}`
-        return emotes(title, field, detail, color, thumbnail, footer);
-    }
-
-    if(args[0] == 'kick'){
-        let author = msg.guild.members.get(msg.author.id).displayName;
-        if(!memberInfo){
-            return msg.channel.send('You need to mention which user you want to **kick**!');
-        }
-        
-        let title = 'Parry this!'
-        let field = ` ${memberInfo.displayName}, you were kicked at the butt by ${author}`
-        let detail = 'Right to the butt'
-        let color = 'f47a42'
-        let thumbnail = 'https://media.tenor.com/images/27f16871c55a3376fa4bfdd76ac2ab5c/tenor.gif'
-        let footer = `Kicked by ${author}`
-        return emotes(title, field, detail, color, thumbnail, footer);
-    }
-    
-
+   
 
 
     if(args[0] == "clear"){
