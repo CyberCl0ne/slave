13e7c54720e6dd449081726a77a8ceaf175500f0
unknown <raziman.terra@gmail.com>
2019-06-29 12:54:38 +0800

    fixed

--- timedPost.js
+++ timedPost.js
@@ -4,16 +4,28 @@ const foodChannel = msg.guild.channels.find(channel => channel.name == '🍗food
 const catsChannel = msg.guild.channels.find(channel => channel.name == '😻cats');
 const memeChannel = msg.guild.channels.find(channel => channel.id == '576957550251999262');
 
-setTimeout(() => randomPuppy('Catmemes').then(
-    URL => catsChannel.send(URL) 
-  ).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
+setTimeout(() => randomPuppy('Catmemes').then( URL =>{
+  var embed = new Discord.Richembed()
+  .setImage(URL)
+  .setTimestamp()
+  .setColor('24E2E7')
+ return catsChannel.send(embed)
+}).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
 
-  setTimeout(() => randomPuppy('Foodporn').then(
-    URL => foodChannel.send(URL)
-  ).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
+setTimeout(() => randomPuppy('Foodporn').then(URL => {
+  var embed = new Discord.Richembed()
+  .setImage(URL)
+  .setTimestamp('24E2E7')
+  .setColor()
+ return foodChannel.send(embed)
+}).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
 
-  setTimeout(() => randomPuppy('me_irl').then(
-    URL =>  memeChannel.send(URL)
-  ).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
+setTimeout(() => randomPuppy('me_irl').then( URL =>{
+  var embed = new Discord.Richembed()
+  .setImage(URL)
+  .setColor('24E2E7')
+  .setTimestamp()
+ return memeChannel.send(embed)
+}).catch(err => console.log(err)), 1 * 6 * 60 * 60 * 1000);
 
 };
