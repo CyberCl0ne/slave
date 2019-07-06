e0c6b6a1043ba7a7d410fee55014c806ad0d8e16
unknown <raziman.terra@gmail.com>
2019-07-06 13:52:30 +0800

    pls

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const addSchema1 = require('./addSchema.js');
 const timedPost = require('./timedPost.js');
 const colorName = ['hotpink1', 'babyblue1', 'russet1','jade1','bumblebee1','mint1','fossil1','pitchblack1','palewhite1','ferrari1','tiger1','grape1','azure1'];
 const resRole = ['Malaysia', 'Singapore'];
-const config = require('./config.json')
+
 const { inspect } = require('util');
 const joinChannel = new Map();
 
