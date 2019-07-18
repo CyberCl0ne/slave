0747f54063dce5edd736afcc36240d3f84392cfd
unknown <raziman.terra@gmail.com>
2019-07-18 15:32:47 +0800

    update database

---
+++ local/skills.js
@@ -0,0 +1,30 @@
+module.exports = {
+    Swordsmen : {
+        first : {
+            name : "Flaming sword",
+            attack: 0,
+            health: 30,
+            defense : 20,
+            level : 5
+        },
+        second : {
+            name : "Critic Blight",
+            attack: 0,
+            health: 30,
+            defense : 20,
+            level : 10
+        },
+
+    },
+    Archer : {
+        first : {
+            name : "Harow Hex",
+            attack: 0,
+            health: 30,
+            defense : 20
+        }
+    },
+    Rogue : {
+
+    }
+}
